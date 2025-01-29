07598f728c79220361f1edfe8d1a848bfb76a1b4
NanocomStudios <dulararupasinghe@gmail.com>
2025-01-30 05:15:12 +0530

    OS Finally booting from an HDD

--- boot.asm
+++ boot.asm
@@ -1,33 +1,6 @@
 [org 0x7c00]                        
 KERNEL_LOCATION equ 0x1000
-VesaModeInfo equ 0x500
-                                    
-jmp short START
-nop
 
-OEMLabel		db "MIKEBOOT"	; Disk label
-BytesPerSector		dw 512		; Bytes per sector
-SectorsPerCluster	db 1		; Sectors per cluster
-ReservedForBoot		dw 1		; Reserved sectors for boot record
-NumberOfFats		db 2		; Number of copies of the FAT
-RootDirEntries		dw 224		; Number of entries in root dir
-					; (224 * 32 = 7168 = 14 sectors to read)
-LogicalSectors		dw 2880		; Number of logical sectors
-MediumByte		db 0F0h		; Medium descriptor byte
-SectorsPerFat		dw 9		; Sectors per FAT
-SectorsPerTrack		dw 18		; Sectors per track (36/cylinder)
-Sides			dw 2		; Number of sides/heads
-HiddenSectors		dd 0		; Number of hidden sectors
-LargeSectors		dd 0		; Number of LBA sectors
-DriveNo			dw 0		; Drive No: 0
-Signature		db 41		; Drive signature: 41 for floppy
-VolumeID		dd 00000000h	; Volume ID: any number
-VolumeLabel		db "MIKEOS     "; Volume Label: any 11 chars
-FileSystem		db "FAT32   "	; File system type: don't change!
-
-
-
-START:
 mov [BOOT_DISK], dl                 
 
                                     
@@ -53,6 +26,15 @@ int 0x13                ; no error management, do your homework!
 jmp KERNEL_LOCATION                    
  
 BOOT_DISK: db 0
+
+times 446-($-$$) db 0
+
+partitionState  db 0
+CHSBegin        db 02, 03, 00
+partitionType   db 0x0c
+CHSEnd          db 0xfe, 0x3f, 0x81
+LBABegin        db 0x80, 0, 0, 0
+partitionSize   db 0, 0xe8, 0x1f, 0
 
 times 510-($-$$) db 0              
 dw 0xaa55
--- bootFat.asm
+++ bootFat.asm
@@ -2,6 +2,7 @@
 [org 0x7c00]
 
 tmpBuffer equ 0x1000
+KERNEL_LOCATION equ 0x1000
 mov [BOOT_DISK], dl                 
                               
 xor ax, ax                          
@@ -10,156 +11,20 @@ mov ds, ax
 mov bp, 0x8000
 mov sp, bp
 
-mov ax, [0x7c00 + 446 + 8]
-mov [lbaTmp], ax
-mov [d_lba], ax
+mov bx, KERNEL_LOCATION
+mov dh, 35  ;Sectors to read
 
-call loadSector
+mov ah, 0x02
+mov al, dh 
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+mov dl, [BOOT_DISK]
+int 0x13                ; no error management, do your homework!
 
+jmp KERNEL_LOCATION  
 
-mov al, [tmpBuffer + 13]
-mov [secPerClus], al
-
-mov ax, [lbaTmp]
-add ax, [tmpBuffer + 14]
-mov [fatBegin], ax 
-add ax, [tmpBuffer + 36]
-add ax, [tmpBuffer + 36]
-mov [d_lba], ax
-mov [lbaTmp], ax
-
-mov al, [secPerClus]
-mov [blkcnt], al
-
-call loadSector
-
-search_dir:
-	popa
-
-	mov ax, ds			; Root dir is now in [buffer]
-	mov es, ax			; Set DI to this info
-	mov di, tmpBuffer
-
-	mov ax, [secPerClus]
-	mov cx, 16
-	mul cx
-	mov cx, ax	; Search all (224) entries
-	mov ax, 0			; Searching at offset 0
-
-
-next_root_entry:
-	xchg cx, dx			; We use CX in the inner loop...
-
-	mov si, kernelName		; Start searching for kernel filename
-	mov cx, 11
-	rep cmpsb
-	je found_file_to_load		; Pointer DI will be at offset 11
-
-	add ax, 32			; Bump searched entries by 1 (32 bytes per entry)
-
-	mov di, tmpBuffer			; Point to next entry
-	add di, ax
-
-	xchg dx, cx			; Get the original CX back
-	loop next_root_entry
-
-	jmp error
-
-found_file_to_load:
-	
-	clc
-	mov ax, [di + 0x1A]
-	mov [lastClus], ax
-	add ax, word [fatBegin]
-	mov [d_lba], ax
-	mov ax, [di + 0x14]
-	mov [lastClusH], ax
-	add ax, 0
-	mov [d_lbaH], ax
-
-	mov ax, [secPerClus]
-	mov [blkcnt], ax
-
-	call loadSector
-
-	mov ax, [lastClus]
-	cmp ax, 0xFFFF
-	jne loadNextFat
-
-	mov ax, [lastClusH]
-	cmp ax, 0xFFFF
-
-	je loadEnd
-
-loadNextFat:
-	clc
-	mov ax, [lastClus]
-	mov bx, 4
-	mul bx
-	div 
-
-
-	mov ax, [di + 0x14]
-	mov [lastClusH], ax
-	add ax, 0
-	mov [d_lbaH], ax
-	
-loadEnd:
-jmp $
-
-error:
-
-	mov al, 69
-	mov ah, 0xe
-	mov bh, 0
-	mov bl, 7
-	int 10h
-
-	jmp $
-
-loadSector:
-    mov si, DAPACK		; address of "disk address packet"
-    mov ah, 0x42		; AL is unused
-    mov dl, 0x80		; drive number 0 (OR the drive # with 0x80)
-    int 0x13
-    jc short error
-	ret
-
-hddWait:
-	push dx
-	push ax
-    clc
-	mov dx, 0x1f7
-	in al, dx
-	and al, 128
-	cmp al, 128
-	je hddWait
-	
-	pop ax
-	pop dx
-	ret
-
-
-;jmp KERNEL_LOCATION                    
- 
-BOOT_DISK:	db	0
-kernelName: db	"KERNEL  BIN"
-lbaTmp:		dw	0
-fatBegin:	dw	0
-secPerClus: db 	0
-lastClus:	dw	0
-lastClusH:	dw	0
-
-DAPACK:
-	db	0x10
-	db	0
-blkcnt:	dw	1		; int 13 resets this to # of blocks actually read/written
-db_add:	dw	0x1000		; memory buffer destination address (0:7c00)
-	dw	0		; in memory page zero
-d_lba:	dw	0	
-d_lbaH:	dw	0	; put the lba to read in this spot
-	dd	0		; more storage bytes only for big lba's ( > 4 bytes )
-
+BOOT_DISK: db 0
 
 times 446-($-$$) db 0
 
@@ -172,3 +37,5 @@ partitionSize   db 0, 0xe8, 0x1f, 0
 
 times 510-($-$$) db 0  
 dw 0xaa55
+
+fatBuffer:
